448e79626ba37722505ae55f6d78e7e51dc92420
Ginny Thomas <90277886+GinnyThomas@users.noreply.github.com>
2026-05-20 15:17:09 +0100

    create show page for articles with a display of the article title and description

--- db/migrate/20260520125702_create_articles.rb
+++ db/migrate/20260520125702_create_articles.rb
@@ -3,7 +3,8 @@ class CreateArticles < ActiveRecord::Migration[8.1]
     create_table :articles do |t|
         t.string :title
         t.text :description
-      t.timestamps
+        t.timestamps :created_at
+        t.timestamps :updated_at
     end
   end
 end
